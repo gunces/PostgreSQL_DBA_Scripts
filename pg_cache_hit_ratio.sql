49c079869cd0161117a11959b2914e11308ea017
gunceS <30113905+gunces@users.noreply.github.com>
2020-12-22 15:07:17 +0300

    Create pg_cache_hit_ratio.sql

---
+++ pg_cache_hit_ratio.sql
@@ -0,0 +1,8 @@
+-- Postgres cache hit ratio
+
+SELECT 
+  sum(heap_blks_read) as heap_read,
+  sum(heap_blks_hit)  as heap_hit,
+  sum(heap_blks_hit) / (sum(heap_blks_hit) + sum(heap_blks_read)) as ratio
+FROM 
+  pg_statio_user_tables;
