b3627c3e3bd84231f63eecaa8a94027120244f28
guncekaya <30113905+gunces@users.noreply.github.com>
2021-05-31 16:36:43 +0300

    Update check_postgres_service_is_up.sh

--- check_postgres_service_is_up.sh
+++ check_postgres_service_is_up.sh
@@ -6,7 +6,7 @@ SQL=`psql -h <host> -p <port> -c "select 1;"`
 
 if [ $? -eq 0 ]; then
     echo OK
-    mail -s "Test Subject" gunce.sarikaya@xxxx.xxx.tr  < /dev/null
+    mail -s "Test Subject" gunce.xxxx@xxxx  < /dev/null
 else
     echo FAIL
 fi
